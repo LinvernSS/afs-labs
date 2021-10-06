021978897552cebe83d19f910449972c4bd1e64d
Invernizzi <lucas.invernizzi@accenturefederal.com>
2021-10-06 11:27:39 -0400

    changed tests

--- featuretests.py
+++ featuretests.py
@@ -3,7 +3,6 @@ import os
 import sys
 from server import app
 from model import connect_to_db, db, example_data, Product
-import json
 
 current = os.getcwd()
 current = current.split("/")
@@ -14,8 +13,6 @@ sys.path.append(current)
 class FlaskTests(unittest.TestCase):
 
     def setUp(self):
-        """Before tests."""
-
         self.client = app.test_client()
         app.config['TESTING'] = True
         # Connect to test database
@@ -27,14 +24,10 @@ class FlaskTests(unittest.TestCase):
         self.cart = Product.query.filter(Product.product_id.in_([1])).all()
 
     def tearDown(self):
-        """For after test."""
-
         db.session.close()
         db.drop_all()
 
     def test_register(self):
-        """Register test user"""
-
         result = self.client.post("/register",
                                   data={"first_name": "Jane",
                                         "last_name": "Doe",
@@ -47,8 +40,6 @@ class FlaskTests(unittest.TestCase):
         self.assertIn(b'register_img', result.data)
 
     def test_login(self):
-        """Test login functionality"""
-
         login = self.client.post("/login",
                                  data={"email": "Jane@jane.com", "password": "password"},
                                  follow_redirects=True)
@@ -58,6 +49,11 @@ class FlaskTests(unittest.TestCase):
         self.assertIn(b"Account", logged_in.data)
         self.assertIn(b"Log Out", logged_in.data)
 
+        result = self.client.post('/save-recipe',
+                                  json={'url': 'http://foodandstyle.com/2012/12/20/persimmon-cosmopolitan/'},
+                                  follow_redirects=True)
+        self.assertIn(b'Success', result.data)
+
         result = self.client.get('/account')
         self.assertIn(b'Jane Doe', result.data)
 
@@ -66,22 +62,16 @@ class FlaskTests(unittest.TestCase):
         self.assertIn(b"Login", logout.data)
 
     def test_frontpage(self):
-        """Test static frontpage"""
-
         result = self.client.get("/")
 
         self.assertIn(b"Farm", result.data)
 
     def test_all_products(self):
-        """Test products listings"""
-
         result = self.client.get("/products")
 
         self.assertIn(b"Add to Cart</button>", result.data)
 
     def test_product_page(self):
-        """Test individual product page"""
-
         result = self.client.get("/products/1")
 
         self.assertIn(b"Blackberries", result.data)
@@ -89,8 +79,22 @@ class FlaskTests(unittest.TestCase):
         result = self.client.post('/products/1', data={'productId': 1}, follow_redirects=True)
         self.assertIn(b'Blackberries', result.data)
 
-    def test_cart_page(self):
-        """Test cart page"""
+    def test_locations(self):
+        result = self.client.get("/locations")
+
+        self.assertIn(b'<div id="map">', result.data)
+
+    def test_search(self):
+        result = self.client.get("/search?terms=black")
+
+        self.assertIn(b"Organic Blackberries", result.data)
+
+    def test_cart_and_checkout(self):
+        result = self.client.post('/add-item', json={'product_id': 1}, follow_redirects=True)
+        self.assertIn(b'Success!', result.data)
+
+        result = self.client.post('/update-cart', json={'product_id': 1, 'qty': 3}, follow_redirects=True)
+        self.assertIn(b'Success', result.data)
 
         result = self.client.get("/cart")
 
@@ -102,36 +106,6 @@ class FlaskTests(unittest.TestCase):
                                                     'zipcode': '31626'}},
                                   follow_redirects=True)
         self.assertIn(b'Success', result.data)
-
-    def test_locations(self):
-        """Test locations page"""
-
-        result = self.client.get("/locations")
-
-        self.assertIn(b'<div id="map">', result.data)
-
-    def test_search(self):
-        """Test search functionality"""
-
-        result = self.client.get("/search?terms=black")
-
-        self.assertIn(b"Organic Blackberries", result.data)
-
-    def test_save_recipe(self):
-        result = self.client.post('/save-recipe',
-                                  json={'url': 'http://foodandstyle.com/2012/12/20/persimmon-cosmopolitan/'},
-                                  follow_redirects=True)
-        self.assertIn(b'Success', result.data)
-
-    def test_update_cart(self):
-        result = self.client.post('/add-item', json={'product_id': 1}, follow_redirects=True)
-        self.assertIn(b'Success!', result.data)
-
-        result = self.client.post('/update-cart', json={'product_id': 1, 'qty': 3}, follow_redirects=True)
-        self.assertIn(b'Success', result.data)
-
-    def test_checkout(self):
-        self.client.session['cart']['1'] = 1
 
         result = self.client.get('/checkout')
         self.assertIn(b'Thank you', result.data)
